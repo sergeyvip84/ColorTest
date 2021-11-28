52e7731e898655dc9c988de84cde9dc5f0010601
sergeyvip84 <87651037+sergeyvip84@users.noreply.github.com>
2021-11-28 21:30:29 +0200

    new project

---
+++ ColorTest/CoincidenceTableView.swift
@@ -0,0 +1,51 @@
+//
+//  CoincidenceTableView.swift
+//  ColorTest
+//
+//  Created by Serhii Palamarchuk on 28.11.2021.
+//
+
+import UIKit
+
+class CoincidenceTableView: UITableViewController {
+
+    var arrayCoincidence = [97,96,99,94,95,83,93,55,46,98]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+    // MARK: - Table view data source
+
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return arrayCoincidence.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = String(arrayCoincidence[indexPath.row])
+        
+        switch arrayCoincidence[indexPath.row] {
+        case 97...100: cell.imageView?.image = UIImage(named: "stars3")
+        case 94..<97: cell.imageView?.image = UIImage(named: "stars2")
+        case 90..<94: cell.imageView?.image = UIImage(named: "star1")
+        default: break
+        }
+        
+        
+        return cell
+    }
+
+   
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+   // override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+//    }
+
+}
--- ColorTest/ViewController.swift
+++ ColorTest/ViewController.swift
@@ -8,12 +8,70 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var sliderR: UISlider!
+    @IBOutlet weak var sliderG: UISlider!
+    @IBOutlet weak var sliderB: UISlider!
+    @IBOutlet weak var labelRGB: UILabel!
+    @IBOutlet weak var randomScreen: UIView!
+    @IBOutlet weak var mainScreen: UIView!
+    @IBOutlet weak var labelPercentOfWin: UILabel!
+    
+    
+    
+    var colorR : CGFloat = 0.5
+    var colorG : CGFloat = 0.5
+    var colorB : CGFloat = 0.5
 
+    let randomRed = CGFloat.random(in: 0...1)
+    let randomGreen = CGFloat.random(in: 0...1)
+    let randomBlue = CGFloat.random(in: 0...1)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        randomScreen.layer.cornerRadius = 15
+        mainScreen.layer.cornerRadius = 15
+        
+        sliderR.value = 0.5
+        sliderG.value = 0.5
+        sliderB.value = 0.5
+          
+        randomScreen.backgroundColor = UIColor(red: randomRed,
+                                               green: randomGreen,
+                                               blue: randomBlue,
+                                               alpha: 1.0)
     }
-
-
+    
+    func viewMainScreen () {
+        mainScreen.backgroundColor = UIColor(red: colorR, green: colorG, blue: colorB, alpha: 1)
+        labelRGB.text = "Red:\(Int(colorR * 100)) Green:\(Int(colorG * 100)) Blue:\(Int(colorB * 100))"
+    }
+    @IBAction func buttomTake(_ sender: UIButton) {
+        var percentOfWin = abs (colorR - randomRed) + abs (colorG - randomGreen) + abs (colorB - randomBlue)
+            percentOfWin = 100 - percentOfWin * 33.333333333333
+        
+        switch percentOfWin {
+        case 97...100: labelPercentOfWin.text = "Perfect! Coincidence \(Int(percentOfWin))%"
+        case 94..<97: labelPercentOfWin.text = "Good! Coincidence \(Int(percentOfWin))%"
+        case 90..<94: labelPercentOfWin.text = "Not bad! Coincidence \(Int(percentOfWin))%"
+        default: labelPercentOfWin.text = "Bad Coincidence \(Int(percentOfWin))%"
+        }
+    }
+        
+    
+    @IBAction func sliderActionR(_ sender: UISlider) {
+        colorR = CGFloat(sender.value)
+        viewMainScreen()
+    }
+    
+    @IBAction func sliderActionG(_ sender: UISlider) {
+        colorG = CGFloat(sender.value)
+        viewMainScreen()
+    }
+    @IBAction func sliderActionB(_ sender: UISlider) {
+        colorB = CGFloat(sender.value)
+        viewMainScreen()
+    }
+    
 }
 
